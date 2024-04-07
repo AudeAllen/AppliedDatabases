230d3de18878be4204f9a310f05c4d8a1e2ee1c4
Audrey Allen <G00425632@atu.ie>
2024-04-07 15:45:53 +0100

    Next two rows

--- Project/City.py
+++ Project/City.py
@@ -5,21 +5,29 @@ import mysql.connector
 
 
 def main():  
-    sql_Connector()
+ 
 
     print ("In Main") 
 
+    global countryStr 
+    global rownum 
+
+    rownum = 0 
     countryStr = "Enter Country:"
+
     display_menu() 
     while True: 
         print("In main Code")
-        sql_Connector()
+        
            
         Choice = input("Choice:")
 
         if (Choice == "X"):
             break
+        elif (Choice == "x"):
+            break
         elif (Choice == "1"):
+            rownum = 0 
             Country = view_cities(countryStr)
             print (Country)
             break
@@ -46,14 +54,21 @@ def view_cities(n):
     
     sql_Connector()
     print ("In view_cities") 
-   
+
+    global Country   
     Country = input(n) 
+
     print(Country)
-   
-    
+     
+    rownum = 0
+
     cursor = db.cursor()
     
-    cursor.execute("SELECT CO.NAME AS 'Country Name', ci.name as 'City Name', ci.district as 'City District', ci.population as 'City Population' FROM COUNTRY CO INNER JOIN CITY CI ON CO.CODE = CI.COUNTRYCODE  where CO.NAME =%s order by ci.name limit 2", (Country,) )
+    #cursor.execute("SELECT CO.NAME AS 'Country Name', ci.name as 'City Name', ci.district as 'City District', ci.population as 'City Population' FROM COUNTRY CO INNER JOIN CITY CI ON CO.CODE = CI.COUNTRYCODE  where CO.NAME =%s order by ci.name limit 4, 3",(Country,))
+    
+    cursor.execute("SELECT CO.NAME AS 'Country Name', ci.name as 'City Name', ci.district as 'City District', ci.population as 'City Population' FROM COUNTRY CO INNER JOIN CITY CI ON CO.CODE = CI.COUNTRYCODE  where CO.NAME =%s order by ci.name limit %s, 3",(Country,rownum))
+    
+     
 
     if cursor.fetchone():
          print("Yes record found ") 
@@ -66,12 +81,34 @@ def view_cities(n):
  
     results= cursor.fetchall() 
     print(results)   
-    main()   
-
-
+    
     db.close()
     cursor.close()
+    
+    #next_two_rows()
 
+    main()   
+
+## End of Section    
+
+def next_two_rows():
+  
+    print (rownum)
+    
+    rownum = rownum + 2
+
+    print ("If you press q you will return to main menu, any other key will return next two cities of the country you entered")
+
+    while True: 
+        print("In next two rows")
+        
+           
+        Choice = input("Choice:")
+
+        if (Choice == "q"):
+            break
+        elif (Choice != "q"):
+            view_cities(Country)
 
 
 def sql_Connector():
